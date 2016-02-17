a3d0c2f3a6b09770a5ad64eee356bee59fcf09ae
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-02-16 17:03:09 -0700

    Sketched core data structs for representing dedups

---
+++ src/main/scala/edu/arizona/sista/assembly/Representation.scala
@@ -0,0 +1,91 @@
+package edu.arizona.sista.assembly
+
+// shared by all of the "post-mention" containers
+trait Category
+
+///////////////////////////////
+// Entities
+///////////////////////////////
+
+trait Entity extends Category
+
+case class SimpleEntity(id: String, modifications: Set[String]) extends Entity {
+
+  // Copy constructor
+  def copy(
+            id: String = this.id,
+            modifications: Set[String] = this.modifications
+            ): SimpleEntity =  SimpleEntity(id, modifications)
+
+}
+
+object SimpleEntity {
+  // alternate constructor
+  def apply(id: String): SimpleEntity = new SimpleEntity(id, Set.empty[String])
+}
+
+case class ComplexEntity(entities: Set[SimpleEntity]) extends Entity
+
+///////////////////////////////
+// Events
+///////////////////////////////
+
+trait Event extends Category {
+  type I
+  type O
+  def getInput: I
+  def getOutput: O
+}
+
+/** Participants can be either simple or complex entities **/
+case class SimpleEvent(input: Set[Entity], output: Set[Entity], label: String) extends Event {
+
+  type I = Set[Entity]
+  type O = Set[Entity]
+  def getInput: I = input
+  def getOutput: O = output
+
+  // Copy constructor
+  def copy(
+            input: Set[Entity] = this.input,
+            output: Set[Entity] = this.output,
+            label: String = this.label
+            ): SimpleEvent =  new SimpleEvent(input, output, label)
+}
+
+object SimpleEvent {
+  // alternate constructors
+  def apply(input: Entity, output: Entity, label: String): SimpleEvent = new SimpleEvent(Set(input), Set(output), label)
+  def apply(input: Set[Entity], output: Entity, label: String): SimpleEvent = new SimpleEvent(input, Set(output), label)
+  def apply(input: Seq[Entity], output: Entity, label: String): SimpleEvent = new SimpleEvent(input.toSet, Set(output), label)
+  def apply(input: Seq[Entity], output: Seq[Entity], label: String): SimpleEvent = new SimpleEvent(input.toSet, output.toSet, label)
+  def apply(input: Entity, output: Set[Entity], label: String): SimpleEvent = new SimpleEvent(Set(input), output, label)
+  // this transformation is only obvious for a simple entity
+  def apply(input: SimpleEntity, label: String): SimpleEvent = label match {
+    case "Binding" =>
+      val output = ComplexEntity(Set(input))
+      new SimpleEvent(Set(input), Set(output), label)
+    case _ =>
+      val output: Entity = input.copy(modifications = input.modifications ++ Set(label))
+      new SimpleEvent(Set(input), Set(output), label)
+  }
+}
+
+case class Regulation(controller: Entity, controlled: Event, polarity: String) extends Event {
+  // constrain value of polarity
+  require(polarity == Regulation.positive || polarity == Regulation.negative, "Polarity must be \"Positive\" or \"Negative\"")
+
+  type I = Set[Category]
+  type O = Set[Entity]
+  def getInput: I = Set(controller, controlled)
+  def getOutput: O = controlled match {
+    case se:SimpleEvent => se.getOutput
+    // recurse until Set of Entities uncovered
+    case reg:Regulation => reg.getOutput
+  }
+}
+
+object Regulation {
+  val positive = "Positive"
+  val negative = "Negative"
+}
